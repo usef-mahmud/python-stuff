78c7203831485ccb516774334d36df13dbbac807
usefmahmud <usef.mahmoud@outlook.com>
2024-05-14 13:43:57 +0300

    some experiments with escape chars

--- exp.py
+++ exp.py
@@ -1,6 +1,5 @@
 from exp_utils import *
 
-x, y = multi_assign(1,2)
-
-print(x/y)
-print(x//y)
+print('this a string \rand other string \ralso other string here')
+print('some chars, to test\b backspace')
+print('most of people write like so\f and complete their typing')
