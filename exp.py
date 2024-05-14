6b70039754cc5ca369790be69a61317473aa3709
usefmahmud <usef.mahmoud@outlook.com>
2024-05-14 15:10:42 +0300

    some number manipulations

--- exp.py
+++ exp.py
@@ -1,5 +1,3 @@
 from exp_utils import *
 
-print('this a string \rand other string \ralso other string here')
-print('some chars, to test\b backspace')
-print('most of people write like so\f and complete their typing')
+print(.5.as_integer_ratio())
